8d74254605a05b1330e41750b0217fa8d905347c
towienko <01169218@pw.edu.pl>
2023-12-09 17:47:52 +0100

    Correct data model to match expert requirements

--- app/app/sql/create_test_id_sequences.sql
+++ app/app/sql/create_test_id_sequences.sql
@@ -1,6 +1,5 @@
 CREATE SEQUENCE IF NOT EXISTS bike_test_seq START WITH 10001
 CREATE SEQUENCE IF NOT EXISTS classification_test_seq START WITH 10001
-CREATE SEQUENCE IF NOT EXISTS classificationaccesslimiter_test_seq START WITH 10001
 CREATE SEQUENCE IF NOT EXISTS race_test_seq START WITH 10001
 CREATE SEQUENCE IF NOT EXISTS racebonus_test_seq START WITH 10001
 CREATE SEQUENCE IF NOT EXISTS raceparticipation_test_seq START WITH 10001
@@ -8,7 +7,6 @@ CREATE SEQUENCE IF NOT EXISTS season_test_seq START WITH 10001
 CREATE SEQUENCE IF NOT EXISTS account_test_seq START WITH 10001
 ALTER TABLE bike MODIFY (id DEFAULT bike_test_seq.nextval)
 ALTER TABLE classification MODIFY (id DEFAULT classification_test_seq.nextval)
-ALTER TABLE classificationaccesslimiter MODIFY (id DEFAULT classificationaccesslimiter_test_seq.nextval)
 ALTER TABLE race MODIFY (id DEFAULT race_test_seq.nextval)
 ALTER TABLE racebonus MODIFY (id DEFAULT racebonus_test_seq.nextval)
 ALTER TABLE raceparticipation MODIFY (id DEFAULT raceparticipation_test_seq.nextval)
