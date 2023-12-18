6320a288867d5ed95c598dc0b01e9c17940d4bed
towienko <01169218@pw.edu.pl>
2023-12-18 23:51:08 +0100

    Create porper endpoints for uploadD

--- app/app/api/coordinator/race.py
+++ app/app/api/coordinator/race.py
@@ -1,4 +1,12 @@
-from fastapi import APIRouter, Depends, HTTPException
+import shutil
+import uuid
+import imghdr
+from io import BytesIO
+
+
+from fastapi import APIRouter, Depends, HTTPException, Request
+from starlette.datastructures import FormData
+
 from sqlmodel import Session, select
 from pydantic import ValidationError
 
@@ -77,6 +85,62 @@ async def create_race(
 
     return RaceReadDetailCoordinator.from_orm(race)
 
+
+@router.post("/create/upload-route/")
+async def create_upload_route(request: Request) -> dict[str, str]:
+    form: FormData = await request.form()
+    tmp_path = form.get('fileobj.path')
+
+    with open(tmp_path, 'r') as f:
+        content = f.read(43)
+        if content.startswith('<?xml version="1.0" encoding="UTF-8"?>\n<gpx'):
+            print('GPX')
+        else:
+            print('INVALID TYPE')
+            raise HTTPException(400)
+
+    new_name = f"{str(uuid.uuid4())}.gpx"
+    new_path =  f'/attachments/{new_name}'
+    shutil.move(tmp_path, new_path)
+
+    return {
+        k: form.get(k) for k in (  # type: ignore[misc]
+            'fileobj.content_type',
+            'fileobj.md5',
+            'fileobj.size',
+            'name')
+    } | {
+        'fileobj.name': new_name,
+        'fileobj.path': new_path
+    }
+
+
+@router.post("/create/upload-graphic/")
+async def create_upload_graphic(request: Request) -> dict[str, str]:
+    form: FormData = await request.form()
+    tmp_path = form.get('fileobj.path')
+
+    extension = imghdr.what(tmp_path)
+    if extension in ('jpeg', 'png'):
+        print(extension)
+    else:
+        print('INVALID TYPE')
+        raise HTTPException(400)
+
+    new_name = f"{str(uuid.uuid4())}.{extension}"
+    new_path =  f'/attachments/{new_name}'
+    shutil.move(tmp_path, new_path)
+
+    return {
+        k: form.get(k) for k in (  # type: ignore[misc]
+            'fileobj.content_type',
+            'fileobj.md5',
+            'fileobj.size',
+            'name')
+    } | {
+        'fileobj.name': new_name,
+        'fileobj.path': new_path
+    }
 
 @router.patch("/{id}")
 async def update_race(
